c128ed6601c968d1a705938f2ac158ec1273a3d9
Keith Filkins <kafilkins@me.com>
2020-12-03 21:02:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,13 @@
 katz_deli = []
 
-def line(katz_deli)
+def line(x)
+  line_array = []
+  if x.length == 0 
+    puts "The line is currently empty."
+  else
+    x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
 
   end
 end
